dda45f0465871b9fad4b5c38f8b57a28b3ef3039
Ivan Ermacoff <jermacoff@gmail.com>
2022-05-08 17:34:43 +0700

    Add Иерархия транспортных средств

---
+++ ООП. Наследование/Иерархия транспортных средств.py
@@ -0,0 +1,102 @@
+class Transport:
+    pass
+
+
+class Water(Transport):
+    pass
+
+
+class Commercial(Water):
+    pass
+
+
+class CruiseLiner(Commercial):
+    pass
+
+
+class Private(Water):
+    pass
+
+
+class Jetski(Private):
+    pass
+
+
+class SpeedBoat(Private):
+    pass
+
+
+class Airborne(Transport):
+    pass
+
+
+class Plane(Airborne):
+    pass
+
+
+class Jet(Plane):
+    pass
+
+
+class Fighter(Jet):
+    pass
+
+
+class Airliner(Jet):
+    pass
+
+
+class Propeller(Plane):
+    pass
+
+
+class SmallAircraft(Propeller):
+    pass
+
+
+class Helicopter(Airborne):
+    pass
+
+
+class Personal(Helicopter):
+    pass
+
+
+class Military(Helicopter):
+    pass
+
+
+class Cargo(Helicopter):
+    pass
+
+
+class Ground(Transport):
+    pass
+
+
+class Space(Transport):
+    pass
+
+
+class Station(Space):
+    pass
+
+
+class International(Station):
+    pass
+
+
+class Local(Station):
+    pass
+
+
+class Ship(Space):
+    pass
+
+
+class Shuttle(Ship):
+    pass
+
+
+class EscapePod(Ship):
+    pass
